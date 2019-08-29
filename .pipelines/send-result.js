9f860d42392ccd882bdf6137bd58bca4e39bca1b
Harry F <harry.f@elanco.com>
2019-08-29 14:35:24 +0100

    added branch name for future bot

--- .pipelines/send-result.js
+++ .pipelines/send-result.js
@@ -1,16 +1,12 @@
 const request = require('request-promise')
 
-const [,,url, type, id, payload] = process.argv
+const [,,url, type, id, branch, payload] = process.argv
 
 console.log({url, type, payload})
 
 async function main() {
 	const resp = await request.post(url, {
-		body: {
-			id,
-			payload,
-			type
-		},
+		body: {id, payload, type, branch},
 		json: true
 	})
 	console.log(resp)
